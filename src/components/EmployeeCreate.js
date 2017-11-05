29dd1025f03d2534ee50b3ef1b870018ed8af736
Arthur BABEY <git@ababey.com>
2017-11-05 23:21:06 +0100

    Employee #1

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { CardSection, Card, Button, Input } from './common';
 
 class EmployeeCreate extends Component {
+  propChanged(prop, value) {
+    this.props.employeeUpdate({ prop, value });
+  }
+
   render() {
     return (
       <View>
         <Card>
           <CardSection>
-            <Input label="Name" placeholder="John" />
+            <Input
+              label="Name"
+              value={this.props.name}
+              placeholder="John"
+              onChangeText={this.propChanged.bind(this, 'name')}
+            />
           </CardSection>
           <CardSection>
-            <Input label="Phone" placeholder="+33 X XX XX XX XX" />
+            <Input
+              label="Phone"
+              value={this.props.phone}
+              placeholder="+33 X XX XX XX XX"
+              onChangeText={this.propChanged.bind(this, 'phone')}
+            />
           </CardSection>
           <CardSection>
           </CardSection>
@@ -24,4 +41,17 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+EmployeeCreate.propTypes = {
+  name: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  shift: PropTypes.string.isRequired,
+  employeeUpdate: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
